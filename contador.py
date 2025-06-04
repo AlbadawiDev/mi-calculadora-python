4b6609cb3bdaa7e9a17e6f71093a9ff78aef3fc0
daniel <tu@email.com>
2025-06-03 22:51:48 -0400

    Elimina scripts de práctica que trasladaré a repos separados

--- contador.py
+++
@@ -1,4 +0,0 @@
-# contador.py
-limite = int(input("Número hasta el que contar: "))
-for i in range(1, limite + 1):
-    print(i)
--- edad.py
+++
@@ -1,6 +0,0 @@
-# edad.py
-edad = int(input("¿Cuántos años tienes? "))
-if edad >= 18:
-    print("Eres mayor de edad.")
-else:
-    print("No eres mayor de edad.")
